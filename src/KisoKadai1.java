377bb01a533fe12f4ea5049098c1dbce9b0881e5
Banshow <the.legend.of.shown@docomo.ne.jp>
2016-01-25 14:58:56 +0900

    a

--- src/KisoKadai1.java
+++ src/KisoKadai1.java
@@ -11,14 +11,22 @@ public class KisoKadai1 {
 	/**
 	 * @param args
 	 */
+
+	//自由にレンジを変える縦と横の長さ
+	static int tate = 9;
+	static int yoko = 9;
+
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-		//自由にレンジを変える縦と横の長さ
-		int tate = 9;
-		int yoko = 9;
-
 		int y = 0;
+
+		//最大の桁数をまず出す
+		int intKeta = tate * yoko;
+
+		String mojiKeta = "" + intKeta;
+
+		int saidaiKeta = mojiKeta.length();
 
 		for( int i = 0; i < tate; i++ ){
 
@@ -30,9 +38,15 @@ public class KisoKadai1 {
 
 				x++;
 
-				if( x * y < 10 ){
+				if( Keta( x, y ) < saidaiKeta ){
 
-					System.out.print( "0"  + y * x + " " );
+					for( int k = 0; k < saidaiKeta - Keta( x, y ); k++ ){
+
+						System.out.print( "0" );
+
+					}
+
+					System.out.print( y * x + " " );
 
 				}else{
 
@@ -45,6 +59,22 @@ public class KisoKadai1 {
 			System.out.println();
 
 		}
+	}
+
+	//縦 * 横の値の桁数を出すメソッド
+	public static int Keta( int tate, int yoko ){
+
+		int intKeta = tate * yoko;
+
+		String mojiKeta = "" + intKeta;
+
+		int answerKeta = mojiKeta.length();
+
+		//デバッグ
+//		System.out.println(  );
+//		System.out.println( saidaiKeta );
+
+		return answerKeta;
 
 	}
 
